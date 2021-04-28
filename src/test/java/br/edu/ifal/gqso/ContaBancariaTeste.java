3fee4655d7a3437847687c6d9ade74863c27e174
AllanaKaroline <akcl1@aluno.ifal.edu.br>
2021-04-28 18:59:06 +0000

    Método saqueTest

--- src/test/java/br/edu/ifal/gqso/ContaBancariaTeste.java
+++ src/test/java/br/edu/ifal/gqso/ContaBancariaTeste.java
@@ -4,16 +4,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import jdk.jfr.Timestamp;
+
 public class ContaBancariaTeste {
     @Test
     public void depositoTest(double valor_dep, double saldo_inicial){
         assertEquals((valor_dep+ saldo_inicial)==valor_dep);
         }
 
-        @Test
-        public void saldoTest(){
-            assertEquals(saldo>=(saldo_inicial));
+    @Test
+    public void saldoTest(){
+        assertEquals(saldo>=(saldo_inicial));
         }
+    @Test
+    public void saqueTest(double valor){
+        assertEquals(valor<(saldo));
+    }
+    }
+        
     }
 
     
