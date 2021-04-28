2312582d69f0c90b3eff3269dd27ca85e99d01a9
AllanaKaroline <82412816+AllanaKaroline@users.noreply.github.com>
2021-04-28 16:30:31 -0300

    Update ContaBancariaTeste.java
    Classe ContaBancariaTeste atualizada

--- src/test/java/br/edu/ifal/gqso/ContaBancariaTeste.java
+++ src/test/java/br/edu/ifal/gqso/ContaBancariaTeste.java
@@ -4,25 +4,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import jdk.jfr.Timestamp;
 
 public class ContaBancariaTeste {
     @Test
-    public void depositoTest(double valor_dep, double saldo_inicial){
-        assertEquals((valor_dep+ saldo_inicial)==valor_dep);
+     public void depositoTest(double valor_dep, double saldo_inicial){
+        assertEquals(valor_dep, valor_dep+saldo_inicial);
         }
 
     @Test
-    public void saldoTest(){
-        assertEquals(saldo>=(saldo_inicial));
+    public void saldoTest(double saldo_final, double saldo_inicial, double valor_dep, double valor_saque){
+        assertEquals(saldo_final, saldo_inicial+valor_dep);
+        assertEquals(saldo_final, saldo_inicial-valor_saque);
+        assertEquals(saldo_final, saldo_final>=saldo_inicial);
         }
     @Test
-    public void saqueTest(double valor){
-        assertEquals(valor<(saldo));
+    public void saqueTest(double valor_saque, double saldo_final, double saldo_inicial){
+        assertEquals(saldo_final, saldo_inicial-valor_saque);
+        assertEquals(valor_saque, valor_saque<=saldo_inicial);
     }
     }
-        
-    }
+    
 
     
 
